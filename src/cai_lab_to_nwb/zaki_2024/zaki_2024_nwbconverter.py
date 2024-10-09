aecf217b1d87f876cae1c54897c7849c282c28f6
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-10-09 17:29:00 +0200

    add minian interface

--- src/cai_lab_to_nwb/zaki_2024/zaki_2024_nwbconverter.py
+++ src/cai_lab_to_nwb/zaki_2024/zaki_2024_nwbconverter.py
@@ -1,18 +1,14 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
 from neuroconv.datainterfaces import (
-    SpikeGLXRecordingInterface,
-    PhySortingInterface,
+    MinianSegmentationInterface,
 )
 
-from cai_lab_to_nwb.zaki_2024 import Embargo2024BehaviorInterface
 
 
-class Embargo2024NWBConverter(NWBConverter):
+class Zaki2024NWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
-        Recording=SpikeGLXRecordingInterface,
-        Sorting=PhySortingInterface,
-        Behavior=Embargo2024BehaviorInterface,
+        MinianSegmentation = MinianSegmentationInterface,
     )
