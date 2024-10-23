383b9aace045fd6ba6585789c054af919f210b09
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-10-23 11:13:14 +0200

    add channel dictionary

--- src/cai_lab_to_nwb/zaki_2024/interfaces/zaki_2024_edf_interface.py
+++ src/cai_lab_to_nwb/zaki_2024/interfaces/zaki_2024_edf_interface.py
@@ -5,56 +5,56 @@ from neuroconv.basedatainterface import BaseDataInterface
 from pynwb import NWBFile, TimeSeries
 from pynwb.device import Device
 
+
 class Zaki2024EDFInterface(BaseDataInterface):
-    
+
     def __init__(self, file_path: FilePath, verbose: bool = False):
-        
+
         self.file_path = Path(file_path)
         self.verbose = verbose
         super().__init__(file_path=file_path)
 
     def add_to_nwbfile(self, nwbfile: NWBFile, **conversion_options) -> NWBFile:
-        
+
+        channels_dict = {
+            "Temp": {
+                "name": "TemperatureSignal",
+                "description": "Temperature signal recorder with HD-X02 wireless telemetry probe",
+                "unit": "celsius",
+            },
+            "EEG": {
+                "name": "EEGSignal",
+                "description": "EEG signal recorder with HD-X02 wireless telemetry probe",
+                "unit": "volts",
+            },
+            "EMG": {
+                "name": "EMGSignal",
+                "description": "EMG signal recorder with HD-X02 wireless telemetry probe",
+                "unit": "volts",
+            },
+            # TODO: Figure out if the units of activity are correct, the raw format marks Volts
+            # TODO: My own reading of the header indicates that the physical units is counts
+            "Activity": {
+                "name": "ActivitySignal",
+                "description": "Activity signal recorder with HD-X02 wireless telemetry probe. It refers to the motion of the probe relative to the receiver and it can be used as a proxy for locomotion.",
+                "unit": "n.a.",
+            },
+        }
         from mne.io import read_raw_edf
 
         edf_reader = read_raw_edf(input_fname=self.file_path, verbose=self.verbose)
-                
-        channels_of_interest = ["Temp", "EEG", "EMG", "Activity"]
-        data, times = edf_reader.get_data(picks=channels_of_interest, return_times=True)
+        data, times = edf_reader.get_data(picks=list(channels_dict.keys()), return_times=True)
         data = data.astype("float32")
-        
-        # Add temperature data to NWBFile
-        temperature = data[0]
-        temperature_time_series = TimeSeries(name="TimeSeriesTemperature", data=temperature, unit="Celsius", timestamps=times)
-        
-        nwbfile.add_acquisition(temperature_time_series)
-        
-        EEG = data[1]
-        EMG = data[2]
-        
-        # Add EEG and EMG data to NWBFile
-        eeg_time_series = TimeSeries(name="TimeSeriesSeriesEEG", data=EEG, unit="V", timestamps=times)
-        emg_time_series = TimeSeries(name="TimeSeriesSeriesEMG", data=EMG, unit="V", timestamps=times)
-        
-        nwbfile.add_acquisition(eeg_time_series)
-        nwbfile.add_acquisition(emg_time_series)
-        
-        # Add Activity
-        
-        activity = data[3]
-        
-        # TODO: Figure out if the units of activity are correct, the raw format marks Volts
-        # TODO: My own reading of the header indicates that the physical units is counts
-        activity_time_series = TimeSeries(name="TimeSeriesActivity", data=activity, unit="n.a.", timestamps=times)
-        
-        nwbfile.add_acquisition(activity_time_series)
-        
+        for channel_index, channel_name in enumerate(channels_dict.keys()):
+            time_series_kwargs = channels_dict[channel_name].copy()
+            time_series_kwargs.update(data=data[channel_index], timestamps=times)
+            time_series = TimeSeries(**time_series_kwargs)
+            nwbfile.add_acquisition(time_series)
+
         # Add device
         description = "Wireless telemetry probe used to record EEG, EMG, temperature, and activity data"
         name = "HD-X02, Data Science International"
         device = Device(name=name, description=description)
         nwbfile.add_device(device)
-        
-        return nwbfile
-        
 
+        return nwbfile
