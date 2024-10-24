3f93b59669b70f1ec6102a99a96c154c44583dcb
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-10-24 17:46:57 +0200

    add checks for data stream existence

--- src/cai_lab_to_nwb/zaki_2024/zaki_2024_convert_session.py
+++ src/cai_lab_to_nwb/zaki_2024/zaki_2024_convert_session.py
@@ -4,10 +4,24 @@ from pathlib import Path
 from typing import Union
 from datetime import datetime
 import pandas as pd
-
+import json
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 
 from zaki_2024_nwbconverter import Zaki2024NWBConverter
+
+
+def get_miniscope_folder_path(folder_path: Union[str, Path]):
+    folder_path = Path(folder_path)
+    if folder_path.is_dir():
+        general_metadata_json = folder_path / "metaData.json"
+        assert general_metadata_json.exists(), f"General metadata json not found in {folder_path}"
+        with open(general_metadata_json) as f:
+            general_metadata = json.load(f)
+        miniscope_name = general_metadata["miniscopes"][0]
+        return folder_path / miniscope_name.replace(" ", "_")
+    else:
+        print("No Miniscope data found at {}".format(folder_path))
+        return None
 
 
 def session_to_nwb(
@@ -15,8 +29,6 @@ def session_to_nwb(
     output_dir_path: Union[str, Path],
     subject_id: str,
     session_id: str,
-    date_str: str,
-    time_str: str,
     stub_test: bool = False,
 ):
 
@@ -31,36 +43,57 @@ def session_to_nwb(
     source_data = dict()
     conversion_options = dict()
 
-    raw_data_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id
+    session_times_file_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_SessionTimes.csv")
+    df = pd.read_csv(session_times_file_path)
+    session_row = df[df["Session"] == task].iloc[0]
+    date_str = session_row["Date"]
+    time_str = session_row["Time"]
+
+    experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Sessions")
 
     # Add Imaging
-    miniscope_folder_path = (
-        raw_data_dir_path / (subject_id + "_Sessions") / session_id / date_str / time_str / "miniscope"
-    )
-    source_data.update(dict(MiniscopeImaging=dict(folder_path=miniscope_folder_path)))
-    conversion_options.update(dict(MiniscopeImaging=dict(stub_test=stub_test)))
+    folder_path = experiment_dir_path / session_id / date_str / time_str
+    miniscope_folder_path = get_miniscope_folder_path(folder_path)
+    if miniscope_folder_path is not None:
+        source_data.update(dict(MiniscopeImaging=dict(folder_path=miniscope_folder_path)))
+        conversion_options.update(dict(MiniscopeImaging=dict(stub_test=stub_test)))
 
     # Add Segmentation
     minian_folder_path = data_dir_path / "Ca_EEG_Calcium" / subject_id / session_id / "minian"
-    source_data.update(dict(MinianSegmentation=dict(folder_path=minian_folder_path)))
-    # conversion_options.update(dict(MinianSegmentation=dict(stub_test=stub_test)))
+    if minian_folder_path.is_dir():
+        source_data.update(dict(MinianSegmentation=dict(folder_path=minian_folder_path)))
+        # conversion_options.update(dict(MinianSegmentation=dict(stub_test=stub_test)))
+    else:
+        print("No Minian data found at {}".format(minian_folder_path))
 
     # Add Behavioral Video
-    video_file_paths = [raw_data_dir_path / (subject_id + "_Sessions") / session_id / (session_id + ".wmv")]
-    source_data.update(dict(Video=dict(file_paths=video_file_paths)))
-    conversion_options.update(dict(Video=dict(stub_test=stub_test)))
+    video_file_path = experiment_dir_path / session_id / (session_id + ".wmv")
+    if video_file_path.is_file():
+        source_data.update(dict(Video=dict(file_paths=[video_file_path])))
+        conversion_options.update(dict(Video=dict(stub_test=stub_test)))
+    else:
+        print("No behavioral video found at {}".format(video_file_path))
 
     # Add Freezing Analysis output
-    csv_file_path = raw_data_dir_path / (subject_id + "_Sessions") / session_id / (session_id + "_FreezingOutput.csv")
-    source_data.update(dict(FreezingBehavior=dict(file_path=csv_file_path, video_sampling_frequency=30.0)))
-    # conversion_options.update(dict(FreezingBehavior=dict(stub_test=stub_test)))
+    freezing_output_file_path = experiment_dir_path / session_id / (session_id + "_FreezingOutput.csv")
+    if freezing_output_file_path.is_file():
+        source_data.update(
+            dict(FreezingBehavior=dict(file_path=freezing_output_file_path, video_sampling_frequency=30.0))
+        )
+        # conversion_options.update(dict(FreezingBehavior=dict(stub_test=stub_test)))
+    else:
+        print("No freezing output csv file found at {}".format(freezing_output_file_path))
 
     # Add EEG, EMG, Temperature and Activity signals
     # TODO discuss how to slice this data
     datetime_obj = datetime.strptime(date_str, "%Y_%m_%d")
     reformatted_date_str = datetime_obj.strftime("_%m%d%y")
     edf_file_path = data_dir_path / "Ca_EEG_EDF" / (subject_id + "_EDF") / (subject_id + reformatted_date_str + ".edf")
-    source_data.update(dict(EDFSignals=dict(file_path=edf_file_path)))
+    if edf_file_path.is_file():
+        source_data.update(dict(EDFSignals=dict(file_path=edf_file_path)))
+        # conversion_options.update(dict(EDFSignals=dict(stub_test=stub_test)))
+    else:
+        print("No .edf file found at {}".format(edf_file_path))
 
     converter = Zaki2024NWBConverter(source_data=source_data)
 
@@ -94,11 +127,6 @@ if __name__ == "__main__":
     session_id = subject_id + "_" + task
     output_dir_path = Path("D:/cai_lab_conversion_nwb/")
     stub_test = True
-    session_times_file_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_SessionTimes.csv")
-    df = pd.read_csv(session_times_file_path)
-    session_row = df[df["Session"] == task].iloc[0]
-    date_str = session_row["Date"]
-    time_str = session_row["Time"]
 
     session_to_nwb(
         data_dir_path=data_dir_path,
@@ -106,6 +134,4 @@ if __name__ == "__main__":
         stub_test=stub_test,
         subject_id=subject_id,
         session_id=session_id,
-        date_str=date_str,
-        time_str=time_str,
     )
