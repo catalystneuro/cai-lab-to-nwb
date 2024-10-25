799559280189fd8d48865f4263be9d38fb6300c8
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-10-25 18:55:45 +0200

    add minian motion correction interface

--- src/cai_lab_to_nwb/zaki_2024/interfaces/minian_interface.py
+++ src/cai_lab_to_nwb/zaki_2024/interfaces/minian_interface.py
@@ -2,13 +2,19 @@ import zarr
 import warnings
 import numpy as np
 import pandas as pd
+from neuroconv import BaseDataInterface
+from neuroconv.tools import get_module
 
-from roiextractors.extraction_tools import PathType
+from roiextractors.extraction_tools import PathType, DtypeType, get_package
 from neuroconv.datainterfaces.ophys.basesegmentationextractorinterface import BaseSegmentationExtractorInterface
 from roiextractors.segmentationextractor import SegmentationExtractor
+from roiextractors.imagingextractor import ImagingExtractor
 
 from typing import Optional
-from pynwb import NWBFile
+from pynwb import NWBFile, TimeSeries
+from pynwb.ophys import MotionCorrection, CorrectedImageStack, OnePhotonSeries
+
+from explore_segmentation_data import extractor
 
 
 class MinianSegmentationExtractor(SegmentationExtractor):
@@ -250,3 +256,192 @@ class MinianSegmentationInterface(BaseSegmentationExtractorInterface):
             plane_segmentation_name=plane_segmentation_name,
             iterator_options=iterator_options,
         )
+
+
+class _MinianMotionCorrectedVideoExtractor(ImagingExtractor):
+    """An auxiliar extractor to get data from a single Minian motion corrected video (.mp4) file.
+
+    This format consists of a single video (.mp4)
+    """
+
+    extractor_name = "_MinianMotionCorrectedVideo"
+
+    def __init__(self, file_path: PathType):
+        """Create a _MinianMotionCorrectedVideoExtractor instance from a file path.
+
+        Parameters
+        ----------
+        file_path: PathType
+           The file path to the Minian motion corrected video (.mp4) file.
+        """
+        from neuroconv.datainterfaces.behavior.video.video_utils import VideoCaptureContext
+
+        self._video_capture = VideoCaptureContext
+        self._cv2 = get_package(package_name="cv2", installation_instructions="pip install opencv-python-headless")
+        self.file_path = file_path
+        super().__init__()
+
+        cap = self._cv2.VideoCapture(str(self.file_path))
+
+        self._num_frames = int(cap.get(self._cv2.CAP_PROP_FRAME_COUNT))
+
+        # Get the frames per second (fps)
+        self._sampling_frequency = cap.get(self._cv2.CAP_PROP_FPS)
+        self.frame_width = int(cap.get(self._cv2.CAP_PROP_FRAME_WIDTH) / 2)
+        self.frame_height = int(cap.get(self._cv2.CAP_PROP_FRAME_HEIGHT))
+
+        _, frame = cap.read()
+        self._dtype = frame.dtype
+
+        # Release the video capture object
+        cap.release()
+
+    def get_num_frames(self) -> int:
+        return self._num_frames
+
+    def get_num_channels(self) -> int:
+        return 1
+
+    def get_image_size(self) -> tuple[int, int]:
+        return (self.frame_height, self.frame_width)
+
+    def get_sampling_frequency(self):
+        return self._sampling_frequency
+
+    def get_dtype(self) -> DtypeType:
+        return self._dtype
+
+    def get_channel_names(self) -> list[str]:
+        return ["OpticalChannel"]
+
+    def get_video(
+        self, start_frame: Optional[int] = None, end_frame: Optional[int] = None, channel: int = 0
+    ) -> np.ndarray:
+        """Get the video frames.
+
+        Parameters
+        ----------
+        start_frame: int, optional
+            Start frame index (inclusive).
+        end_frame: int, optional
+            End frame index (exclusive).
+        channel: int, optional
+            Channel index.
+
+        Returns
+        -------
+        video: numpy.ndarray
+            The video frames.
+
+        Notes
+        -----
+        The grayscale conversion is based on minian
+        https://github.com/denisecailab/minian/blob/f64c456ca027200e19cf40a80f0596106918fd09/minian/utilities.py#LL272C12-L272C12
+        """
+        if channel != 0:
+            raise NotImplementedError(
+                f"The {self.extractor_name}Extractor does not currently support multiple color channels."
+            )
+
+        end_frame = end_frame or self.get_num_frames()
+        start_frame = start_frame or 0
+
+        video = np.empty(shape=(end_frame - start_frame, *self.get_image_size()), dtype=self.get_dtype())
+        with self._video_capture(file_path=str(self.file_path)) as video_obj:
+            # Set the starting frame position
+            video_obj.current_frame = start_frame
+            for frame_number in range(end_frame - start_frame):
+                frame = next(video_obj)
+
+                # motion corrected video in minian are saved side by side (right side) with non-corrected video
+                # thus we need to extract only the right side of the frames
+                video[frame_number] = self._cv2.cvtColor(frame[:, self.frame_width :], self._cv2.COLOR_RGB2GRAY)
+
+        return video
+
+
+class MinianMotionCorrectionInterface(BaseDataInterface):
+    """Data interface for adding the motion corrected image data produced by Minian software."""
+
+    display_name = "MinianMotionCorrection"
+    associated_suffixes = (".mp4", ".zarr")
+    info = "Interface for motion corrected imaging data produced by Minian software."
+
+    def __init__(self, folder_path: PathType, verbose: bool = True):
+        """
+        Interface for motion corrected imaging data produced by Minian software.
+
+        Parameters
+        ----------
+        folder_path : PathType
+            Path to .zarr path (Minian output).
+        verbose : bool, default True
+            Whether to print progress
+        """
+        super().__init__(folder_path=folder_path, verbose=verbose)
+
+    def add_to_nwbfile(
+        self,
+        nwbfile: NWBFile,
+        metadata: dict,
+        corrected_image_stack_name: str = "CorrectedImageStack",
+        stub_test: bool = False,
+    ) -> None:
+
+        # extract xy_shift
+        assert "/motion.zarr" in zarr.open(self.folder_path), f"Group '/motion.zarr' not found in the Zarr store."
+        dataset = zarr.open(str(self.folder_path) + "/motion.zarr")
+        # from zarr field motion.zarr/shift_dim we can verify that the two column refer respectively to
+        # ['height','width'] --> ['y','x']. Following best practice we swap the two columns
+        xy_shifts = dataset["motion"][:, [1, 0]]
+
+        # extract corrected image stack
+        motion_corrected_video_file_path = self.folder_path / "minian_mc.mp4"
+        extractor = _MinianMotionCorrectedVideoExtractor(file_path=str(motion_corrected_video_file_path))
+        end_frame = 100 if stub_test else None
+        motion_corrected_data = extractor.get_video(end_frame=end_frame)
+
+        # add motion correction
+        name_suffix = corrected_image_stack_name.replace("CorrectedImageStack", "")
+        original_one_photon_series_name = f"OnePhotonSeries{name_suffix}"
+        assert (
+            original_one_photon_series_name in nwbfile.acquisition
+        ), f"The one photon series '{original_one_photon_series_name}' does not exist in the NWBFile."
+
+        original_one_photon_series = nwbfile.acquisition[original_one_photon_series_name]
+
+        timestamps = original_one_photon_series.timestamps
+        assert timestamps is not None, "The timestamps for the original one photon series must be set."
+        assert len(xy_shifts) == len(timestamps), "The length of the xy shifts must match the length of the timestamps."
+
+        xy_translation = TimeSeries(
+            name="xy_translation",  # name must be 'xy_translation'
+            data=xy_shifts[:end_frame, :],
+            description=f"The x, y shifts for the {original_one_photon_series_name} imaging data.",
+            unit="px",
+            timestamps=timestamps[:end_frame],
+        )
+
+        corrected_one_photon_series = OnePhotonSeries(
+            name="corrected",  # name must be 'corrected'
+            data=motion_corrected_data,
+            imaging_plane=original_one_photon_series.imaging_plane,
+            timestamps=timestamps[:end_frame],
+            unit="n.a.",
+        )
+
+        corrected_image_stack = CorrectedImageStack(
+            name=corrected_image_stack_name,
+            corrected=corrected_one_photon_series,
+            original=original_one_photon_series,
+            xy_translation=xy_translation,
+        )
+
+        ophys = get_module(nwbfile, "ophys")
+        if "MotionCorrection" not in ophys.data_interfaces:
+            motion_correction = MotionCorrection(name="MotionCorrection")
+            ophys.add(motion_correction)
+        else:
+            motion_correction = ophys.data_interfaces["MotionCorrection"]
+
+        motion_correction.add_corrected_image_stack(corrected_image_stack)
