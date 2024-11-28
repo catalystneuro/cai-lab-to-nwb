1c26451084bbf60c55ed465fe80e6ba8800ce93a
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-11-28 14:14:14 +0100

    generalize for other subjects

--- src/cai_lab_to_nwb/zaki_2024/zaki_2024_convert_session.py
+++ src/cai_lab_to_nwb/zaki_2024/zaki_2024_convert_session.py
@@ -4,13 +4,13 @@ import time
 
 from pathlib import Path
 from typing import Union
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 
 from zaki_2024_nwbconverter import Zaki2024NWBConverter
-from utils import get_session_slicing_time_range
+from utils import get_session_slicing_time_range, get_session_run_time
 from interfaces.miniscope_imaging_interface import get_miniscope_folder_path
 
 
@@ -23,11 +23,13 @@ def session_to_nwb(
     time_str: str,
     stub_test: bool = False,
     verbose: bool = True,
-    include_imaging: bool = True,
-    include_freezing_behavior: bool = True,
-    include_sleep_classification: bool = True,
-    include_behavioral_video: bool = True,
-    include_eeg_emg_signals: bool = True,
+    experiment_dir_path: Union[str, Path] = None,
+    imaging_folder_path: Union[str, Path] = None,
+    minian_folder_path: Union[str, Path] = None,
+    video_file_path: Union[str, Path] = None,
+    freezing_output_file_path: Union[str, Path] = None,
+    edf_file_path: Union[str, Path] = None,
+    sleep_classification_file_path: Union[str, Path] = None,
     shock_stimulus: dict = None,
 ):
     print(f"Converting session {session_id}")
@@ -45,81 +47,67 @@ def session_to_nwb(
     source_data = dict()
     conversion_options = dict()
 
-    if "Offline" in session_id:
-        offline_day = session_id.split("Session")[0]
-        experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Offline") / offline_day
-    else:
-        experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Sessions") / session_id
-        include_eeg_emg_signals = False
-        include_sleep_classification = False
+    # Add Miniscope data
+    miniscope_folder_path = None
+    if imaging_folder_path:
+        miniscope_folder_path = get_miniscope_folder_path(imaging_folder_path)
+        assert miniscope_folder_path.is_dir(), f"{miniscope_folder_path} does not exist"
 
-    # Add Imaging
-    folder_path = experiment_dir_path / date_str / time_str
-    miniscope_folder_path = get_miniscope_folder_path(folder_path)
-    if miniscope_folder_path is not None and include_imaging:
         source_data.update(dict(MiniscopeImaging=dict(folder_path=miniscope_folder_path)))
         conversion_options.update(dict(MiniscopeImaging=dict(stub_test=stub_test)))
-    elif verbose and not include_imaging:
-        print(f"Miniscope data will not be included for session {session_id}")
-    elif verbose and miniscope_folder_path is None:
-        print(f"No Miniscope data found at {miniscope_folder_path}")
 
-    # Add Segmentation
-    minian_folder_path = data_dir_path / "Ca_EEG_Calcium" / subject_id / session_id / "minian"
-    if minian_folder_path.is_dir() and include_imaging:
+    # Add Segmentation and Motion Correction
+    if minian_folder_path:
+        assert minian_folder_path.is_dir(), f"{minian_folder_path} does not exist"
         source_data.update(dict(MinianSegmentation=dict(folder_path=minian_folder_path)))
         conversion_options.update(dict(MinianSegmentation=dict(stub_test=stub_test)))
-    elif verbose and not include_imaging:
-        print(f"Minian data will not be included for session {session_id}")
-    elif verbose and not minian_folder_path.is_dir():
-        print(f"No Minian data found at {minian_folder_path}")
 
-    # Add Motion Correction
-    motion_corrected_video = minian_folder_path / "minian_mc.mp4"
-    if motion_corrected_video.is_file() and include_imaging:
-        source_data.update(
-            dict(MinianMotionCorrection=dict(folder_path=minian_folder_path, video_file_path=motion_corrected_video))
-        )
-        conversion_options.update(dict(MinianMotionCorrection=dict(stub_test=stub_test)))
-    elif verbose and not include_imaging:
-        print(f"Minian Motion Correction data will not be included for session {session_id}")
-    elif verbose and not motion_corrected_video.is_file():
-        print(f"No motion corrected data found at {motion_corrected_video}")
+        motion_corrected_video = minian_folder_path / "minian_mc.mp4"
+        if motion_corrected_video.is_file():
+            source_data.update(
+                dict(
+                    MinianMotionCorrection=dict(folder_path=minian_folder_path, video_file_path=motion_corrected_video)
+                )
+            )
+            conversion_options.update(dict(MinianMotionCorrection=dict(stub_test=stub_test)))
+        elif verbose and not motion_corrected_video.is_file():
+            print(f"No motion corrected data found at {motion_corrected_video}")
 
     # Add Behavioral Video
-    video_file_path = experiment_dir_path / (session_id + ".wmv")
-    if video_file_path.is_file() and include_behavioral_video:
+    if video_file_path:
+        assert video_file_path.is_file(), f"{video_file_path} does not exist"
         source_data.update(dict(Video=dict(file_paths=[video_file_path])))
         conversion_options.update(dict(Video=dict(stub_test=stub_test)))
-    elif verbose and not include_behavioral_video:
-        print(f"The behavioral video will not be included for session {session_id}")
-    elif verbose and not video_file_path.is_file():
-        print(f"No behavioral video found at {video_file_path}")
 
     # Add Freezing Analysis output
-    freezing_output_file_path = experiment_dir_path / (session_id + "_FreezingOutput.csv")
-    if freezing_output_file_path.is_file() and include_freezing_behavior:
+    if freezing_output_file_path:
+        assert freezing_output_file_path.is_file(), f"{freezing_output_file_path} does not exist"
         source_data.update(
             dict(FreezingBehavior=dict(file_path=freezing_output_file_path, video_sampling_frequency=30.0))
         )
-    elif verbose and not include_freezing_behavior:
-        print(f"The Freezing Analysis output will not be included for session {session_id}")
-    elif verbose and not freezing_output_file_path.is_file():
-        print(f"No freezing output csv file found at {freezing_output_file_path}")
 
     # Add EEG, EMG, Temperature and Activity signals
-    datetime_obj = datetime.strptime(date_str, "%Y_%m_%d")
-    reformatted_date_str = datetime_obj.strftime("_%m%d%y")
-    edf_file_path = data_dir_path / "Ca_EEG_EDF" / (subject_id + "_EDF") / (subject_id + reformatted_date_str + ".edf")
 
-    if edf_file_path.is_file() and include_eeg_emg_signals:
-        miniscope_metadata_json = folder_path / "metaData.json"
-        assert miniscope_metadata_json.exists(), f"General metadata json not found in {folder_path}"
-        timestamps_file_path = miniscope_folder_path / "timeStamps.csv"
-        assert timestamps_file_path.exists(), f"Miniscope timestamps file not found in {miniscope_folder_path}"
-        start_datetime_timestamp, stop_datetime_timestamp = get_session_slicing_time_range(
-            miniscope_metadata_json=miniscope_metadata_json, timestamps_file_path=timestamps_file_path
-        )
+    if edf_file_path:
+        assert edf_file_path.is_file(), f"{edf_file_path} does not exist"
+        if imaging_folder_path.is_dir() and miniscope_folder_path.is_dir():
+            miniscope_metadata_json = imaging_folder_path / "metaData.json"
+            assert miniscope_metadata_json.exists(), f"General metadata json not found in {imaging_folder_path}"
+            timestamps_file_path = miniscope_folder_path / "timeStamps.csv"
+            assert timestamps_file_path.exists(), f"Miniscope timestamps file not found in {miniscope_folder_path}"
+            start_datetime_timestamp, stop_datetime_timestamp = get_session_slicing_time_range(
+                miniscope_metadata_json=miniscope_metadata_json, timestamps_file_path=timestamps_file_path
+            )
+        else:
+            datetime_str = date_str + " " + time_str
+            start_datetime_timestamp = datetime.strptime(datetime_str, "%Y_%m_%d %H_%M_%S")
+
+            txt_file_path = experiment_dir_path / f"{session_id}.txt"
+            assert txt_file_path.is_file(), f"{txt_file_path} does not exist"
+
+            session_run_time = get_session_run_time(txt_file_path=txt_file_path)
+            stop_datetime_timestamp = start_datetime_timestamp + timedelta(seconds=session_run_time)
+
         source_data.update(
             dict(
                 EDFSignals=dict(
@@ -136,23 +124,13 @@ def session_to_nwb(
                 )
             )
         )
-    elif verbose and not include_eeg_emg_signals:
-        print(f"The EEG, EMG, Temperature and Activity signals will not be included for session {session_id}")
-    elif verbose and not edf_file_path.is_file():
-        print(f"No .edf file found at {edf_file_path}")
 
     # Add Sleep Classification output
-    sleep_classification_file_path = (
-        data_dir_path / "Ca_EEG_Sleep" / subject_id / "AlignedSleep" / (session_id + "_AlignedSleep.csv")
-    )
-    if sleep_classification_file_path.is_file() and include_sleep_classification:
+    if sleep_classification_file_path:
+        assert sleep_classification_file_path.is_file(), f"{sleep_classification_file_path} does not exist"
         source_data.update(
             dict(SleepClassification=dict(file_path=sleep_classification_file_path, video_sampling_frequency=30.0))
         )
-    elif verbose and not include_sleep_classification:
-        print(f"The Sleep Classification output will not be included for session {session_id}")
-    elif verbose and not sleep_classification_file_path.is_file():
-        print(f"No sleep classification output csv file found at {sleep_classification_file_path}")
 
     # Add Shock Stimuli times for FC sessions
     if shock_stimulus is not None:
@@ -199,24 +177,62 @@ if __name__ == "__main__":
 
     # Parameters for conversion
     data_dir_path = Path("D:/")
-    subject_id = "Ca_EEG3-4"
-    task = "FC"
+    subject_id = "Ca_EEG2-1"
+    task = "OfflineDay2Session1"
     session_id = subject_id + "_" + task
     output_dir_path = Path("D:/cai_lab_conversion_nwb/")
     stub_test = False
-    session_times_file_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_SessionTimes.csv")
-    df = pd.read_csv(session_times_file_path)
-    session_row = df[df["Session"] == task].iloc[0]
-    date_str = session_row["Date"]
-    time_str = session_row["Time"]
-    shock_stimulus = dict(shock_times=[120.0, 180.0, 240.0], shock_amplitude=0.25, shock_duration=2.0)
-    session_to_nwb(
+    # session_times_file_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_SessionTimes.csv")
+    # df = pd.read_csv(session_times_file_path)
+    # session_row = df[df["Session"] == task].iloc[0]
+    date_str = "2021_10_14"  # session_row["Date"]
+    time_str = "09_24_27"  # session_row["Time"]
+
+    session_to_nwb_kwargs_per_session = dict(
         data_dir_path=data_dir_path,
         output_dir_path=output_dir_path,
-        stub_test=stub_test,
         subject_id=subject_id,
         session_id=session_id,
         date_str=date_str,
         time_str=time_str,
-        shock_stimulus=shock_stimulus,
+        stub_test=stub_test,
     )
+
+    if "Offline" in session_id:
+        offline_day = session_id.split("Session")[0]
+        experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Offline") / offline_day
+        datetime_obj = datetime.strptime(date_str, "%Y_%m_%d")
+        reformatted_date_str = datetime_obj.strftime("_%m%d%y")
+        edf_file_path = (
+            data_dir_path / "Ca_EEG_EDF" / (subject_id + "_EDF") / (subject_id + reformatted_date_str + ".edf")
+        )
+        sleep_classification_file_path = (
+            data_dir_path / "Ca_EEG_Sleep" / subject_id / "AlignedSleep" / (session_id + "_AlignedSleep.csv")
+        )
+        session_to_nwb_kwargs_per_session.update(
+            edf_file_path=edf_file_path,
+            sleep_classification_file_path=sleep_classification_file_path,
+        )
+    else:
+        experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Sessions") / session_id
+
+        video_file_path = experiment_dir_path / (session_id + ".wmv")
+        freezing_output_file_path = experiment_dir_path / (session_id + "_FreezingOutput.csv")
+        session_to_nwb_kwargs_per_session.update(
+            video_file_path=video_file_path,
+            freezing_output_file_path=freezing_output_file_path,
+        )
+        if "FC" in session_id:
+            shock_stimulus = dict(shock_times=[120.0, 180.0, 240.0], shock_amplitude=1.5, shock_duration=2.0)
+            session_to_nwb_kwargs_per_session.update(
+                shock_stimulus=shock_stimulus,
+            )
+
+    # imaging_folder_path = experiment_dir_path / time_str
+    imaging_folder_path = experiment_dir_path / date_str / time_str
+    minian_folder_path = data_dir_path / "Ca_EEG_Calcium" / subject_id / session_id / "minian"
+    session_to_nwb_kwargs_per_session.update(
+        imaging_folder_path=imaging_folder_path,
+        minian_folder_path=minian_folder_path,
+    )
+    session_to_nwb(**session_to_nwb_kwargs_per_session)
