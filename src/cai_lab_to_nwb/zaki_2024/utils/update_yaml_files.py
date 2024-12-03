67cc7a4e9d683511523e6ee2f90cef84300776c9
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-12-03 18:40:17 +0100

    add doc strings

--- src/cai_lab_to_nwb/zaki_2024/utils/update_yaml_files.py
+++ src/cai_lab_to_nwb/zaki_2024/utils/update_yaml_files.py
@@ -8,6 +8,28 @@ import warnings
 
 
 def get_session_times_df(subject_id: str, data_dir_path: Union[str, Path], session_types: list = ()) -> pd.DataFrame:
+    """
+    Retrieve a DataFrame containing session times for a given subject.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+    session_types : list, optional
+        List of session types to filter. Defaults to an empty list.
+
+    Returns:
+    --------
+    pd.DataFrame
+        A DataFrame with session names, dates, times.
+
+    Raises:
+    -------
+    AssertionError
+        If the session times file does not exist.
+    """
 
     if "Ca_EEG3-" in subject_id:
         session_times_file_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_SessionTimes.csv")
@@ -36,7 +58,30 @@ def get_session_times_df(subject_id: str, data_dir_path: Union[str, Path], sessi
         print(f"Invalid subject_id: {subject_id}")
 
 
-def get_experiment_dir_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]):
+def get_experiment_dir_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]) -> Path:
+    """
+    Construct the path to the experiment directory for a given subject and session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Path
+        The path to the experiment directory.
+
+    Raises:
+    -------
+    AssertionError
+        If the experiment directory does not exist.
+    """
+
     if "Offline" in session_id:
         offline_day = session_id.split("Session")[0]
         experiment_dir_path = data_dir_path / "Ca_EEG_Experiment" / subject_id / (subject_id + "_Offline") / offline_day
@@ -46,8 +91,22 @@ def get_experiment_dir_path(subject_id: str, session_id: str, data_dir_path: Uni
     return experiment_dir_path
 
 
-def get_date_str_from_experiment_dir_path(experiment_dir_path: Union[str, Path]):
-    # get date_str as the name of the folder in experiment_dir_path
+def get_date_str_from_experiment_dir_path(experiment_dir_path: Union[str, Path]) -> Union[str, None]:
+    """
+    Extract the date string from the name of the subdirectory in the experiment directory.
+    This is meant to work only for offline sessions
+
+    Parameters:
+    -----------
+    experiment_dir_path : Union[str, Path]
+        Path to the experiment directory.
+
+    Returns:
+    --------
+    Union[str, None]
+        The date string in the format "YYYY_MM_DD", or None if not found.
+    """
+
     for subdirectory in experiment_dir_path.iterdir():
         if subdirectory.is_dir():
             folder_name = subdirectory.name
@@ -58,7 +117,25 @@ def get_date_str_from_experiment_dir_path(experiment_dir_path: Union[str, Path])
                 print(f"The folder name '{folder_name}' is NOT in the correct date format: '%Y_%m_%d'.")
 
 
-def get_edf_file_path(subject_id: str, date_str: str, data_dir_path: Union[str, Path]):
+def get_edf_file_path(subject_id: str, date_str: str, data_dir_path: Union[str, Path]) -> Union[Path, None]:
+    """
+    Retrieve the path to the EDF file for a given subject and date.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    date_str : str
+        The date string in "YYYY_MM_DD" format.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the EDF file, or None if not found.
+    """
+
     try:
         datetime_obj = datetime.strptime(date_str, "%Y_%m_%d")
         reformatted_date_str = datetime_obj.strftime("_%m%d%y")
@@ -73,7 +150,26 @@ def get_edf_file_path(subject_id: str, date_str: str, data_dir_path: Union[str, 
         print(f"The date_str is not in the correct format: '{date_str}'")
 
 
-def get_sleep_classification_file_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]):
+def get_sleep_classification_file_path(
+    subject_id: str, session_id: str, data_dir_path: Union[str, Path]
+) -> Union[Path, None]:
+    """
+    Retrieve the path to the sleep classification file for a given subject and session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the sleep classification file, or None if not found.
+    """
     sleep_classification_file_path = (
         data_dir_path / "Ca_EEG_Sleep" / subject_id / "AlignedSleep" / (session_id + "_AlignedSleep.csv")
     )
@@ -85,7 +181,25 @@ def get_sleep_classification_file_path(subject_id: str, session_id: str, data_di
     return sleep_classification_file_path
 
 
-def get_video_file_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]):
+def get_video_file_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]) -> Union[Path, None]:
+    """
+    Retrieve the path to the behavioral video file for a given subject and session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the video file, or None if not found.
+    """
+
     experiment_dir_path = get_experiment_dir_path(subject_id, session_id, data_dir_path)
     video_file_path = Path(experiment_dir_path) / (session_id + ".wmv")
     if not video_file_path.is_file():
@@ -94,7 +208,27 @@ def get_video_file_path(subject_id: str, session_id: str, data_dir_path: Union[s
     return video_file_path
 
 
-def get_freezing_output_file_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]):
+def get_freezing_output_file_path(
+    subject_id: str, session_id: str, data_dir_path: Union[str, Path]
+) -> Union[Path, None]:
+    """
+    Retrieve the path to the freezing output file for a given subject and session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the freezing output file, or None if not found.
+    """
+
     experiment_dir_path = get_experiment_dir_path(subject_id, session_id, data_dir_path)
     freezing_output_file_path = Path(experiment_dir_path) / (session_id + "_FreezingOutput.csv")
     if not freezing_output_file_path.is_file():
@@ -105,7 +239,29 @@ def get_freezing_output_file_path(subject_id: str, session_id: str, data_dir_pat
 
 def get_imaging_folder_path(
     subject_id: str, session_id: str, data_dir_path: Union[str, Path], time_str: str, date_str: str
-):
+) -> Union[Path, None]:
+    """
+    Retrieve the path to the imaging folder for a given session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+    time_str : str
+        The session start time.
+    date_str : str
+        The session date.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the imaging folder, or None if not found.
+    """
+
     experiment_dir_path = get_experiment_dir_path(subject_id, session_id, data_dir_path)
     if date_str is not None:
         imaging_folder_path = Path(experiment_dir_path) / date_str / time_str
@@ -117,7 +273,25 @@ def get_imaging_folder_path(
     return imaging_folder_path
 
 
-def get_miniscope_folder_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]):
+def get_miniscope_folder_path(subject_id: str, session_id: str, data_dir_path: Union[str, Path]) -> Union[Path, None]:
+    """
+    Retrieve the path to the miniscope folder for a given subject and session.
+
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    session_id : str
+        The ID of the session.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+
+    Returns:
+    --------
+    Union[Path, None]
+        Path to the miniscope folder, or None if not found.
+    """
+
     minian_folder_path = data_dir_path / "Ca_EEG_Calcium" / subject_id / session_id / "minian"
     if not minian_folder_path.is_dir():
         warnings.warn(f"{minian_folder_path} not found. The miniscope folder will not be added.")
@@ -130,9 +304,28 @@ def update_session_to_nwb_kwargs_yaml(
     data_dir_path: Union[str, Path],
     output_dir_path: Union[str, Path],
     experiment_design_file_path: Union[str, Path],
-    session_types: list = (),  # session_id without the subject_id
+    session_types: list = (),
 ):
+    """
+    Update a YAML file with parameters required for session-to-NWB conversion.
 
+    Parameters:
+    -----------
+    subject_id : str
+        The ID of the subject.
+    data_dir_path : Union[str, Path]
+        Path to the base data directory.
+    output_dir_path : Union[str, Path]
+        Path to the output directory for NWB files.
+    experiment_design_file_path : Union[str, Path]
+        Path to the experiment design file.
+    session_types : list, optional
+        List of session types to process. Defaults to an empty list.
+
+    Returns:
+    --------
+    None
+    """
     yaml_file_path = Path(__file__).parent / "session_to_nwb_kwargs.yaml"
     subjects_df = pd.read_excel(experiment_design_file_path)
     session_times_df = get_session_times_df(
